43b19ea52bc245846468d19257c920e317472307
miguelmacis-afk <miguelmacis@gmail.com>
2025-12-21 11:42:44 +0100

    Update scrape_kick.js

--- scrape_kick.js
+++ scrape_kick.js
@@ -4,13 +4,32 @@ const fs = require('fs');
 (async () => {
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  await page.goto('https://kick.facepunch.com/');
-  await page.waitForLoadState('networkidle');
 
-  const imgs = await page.$$eval('img', imgs => imgs.map(i => i.src)
-    .filter(src => /\.(jpg|jpeg|png|svg)$/i.test(src) && !/logo|icon|banner|avatar|favicon/i.test(src))
-  );
+  await page.goto('https://kick.facepunch.com/', { waitUntil: 'networkidle' });
 
-  fs.writeFileSync('kick_imgs.txt', imgs.join('\n'));
+  const drops = await page.evaluate(() => {
+    const imgs = Array.from(document.querySelectorAll('img'));
+
+    return imgs
+      .map(img => ({
+        src: img.src,
+        w: img.naturalWidth,
+        h: img.naturalHeight
+      }))
+      .filter(img =>
+        img.src &&
+        /\.(jpg|jpeg|png)$/i.test(img.src) &&
+        img.w >= 150 &&
+        img.h >= 150 &&
+        img.w <= 600 &&
+        img.h <= 600 &&
+        !/logo|icon|banner|avatar|favicon|header|background|promo|marque/i.test(img.src)
+      )
+      .map(img => img.src);
+  });
+
+  const unique = [...new Set(drops)];
+  fs.writeFileSync('kick_imgs.txt', unique.join('\n'));
+
   await browser.close();
 })();
