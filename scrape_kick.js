75dea2579103369b22c0fe37262e749658fbae13
miguelmacis-afk <miguelmacis@gmail.com>
2025-12-21 08:33:49 +0100

    Create scrape_kick.js

---
+++ scrape_kick.js
@@ -0,0 +1,16 @@
+const { chromium } = require('playwright');
+const fs = require('fs');
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto('https://kick.facepunch.com/');
+  await page.waitForLoadState('networkidle');
+
+  const imgs = await page.$$eval('img', imgs => imgs.map(i => i.src)
+    .filter(src => /\.(jpg|jpeg|png|svg)$/i.test(src) && !/logo|icon|banner|avatar|favicon/i.test(src))
+  );
+
+  fs.writeFileSync('kick_imgs.txt', imgs.join('\n'));
+  await browser.close();
+})();
